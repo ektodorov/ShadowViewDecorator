a29555f9deea038e54f7255f06f13b72ba215359
ektodorov <ek_todorov@abv.bg>
2015-02-11 23:56:22 +0200

    1.Adding images for readme;

--- ShadowViewDecorator/src/com/blogspot/techzealous/shadowviewdecorator/MainActivity.java
+++ ShadowViewDecorator/src/com/blogspot/techzealous/shadowviewdecorator/MainActivity.java
@@ -5,13 +5,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +20,8 @@ public class MainActivity extends Activity {
 	private Button mButtonRect;
 	private Button mButtonRectRound;
 	private Button mButtonCircle;
+	private Button mButtonArrow;
+	private TextView mTextViewHorizontal;
 	
 	private Resources mResources;
 	private ShadowViewDecorator mDecorator;
@@ -32,6 +34,8 @@ public class MainActivity extends Activity {
 		mButtonRect = (Button)findViewById(R.id.buttonRectLeft);
 		mButtonRectRound = (Button)findViewById(R.id.buttonRectRoundRight);
 		mButtonCircle = (Button)findViewById(R.id.buttonCircle);
+		mButtonArrow = (Button)findViewById(R.id.buttonArrow);
+		mTextViewHorizontal = (TextView)findViewById(R.id.textViewHorizontal);
 		RelativeLayout rl = (RelativeLayout)findViewById(R.id.relativeLayoutMain);
 		
 		mResources = getResources();
@@ -56,7 +60,14 @@ public class MainActivity extends Activity {
 		rl.post(new Runnable() {
 			@Override
 			public void run() {
-				
+				mDecorator.dropShadow(mButtonRect, 4, 4, Color.BLACK, 20, 5);
+				mDecorator.dropShadow(mButtonRectRound, 4, 4, Color.BLACK, 20, 5);
+				mDecorator.dropShadow(mButtonCircle, 4, 4, Color.BLACK, 20, 5);
+				mDecorator.dropShadow(mButtonArrow, 4, 4, Color.BLACK, 20, 5);
+				Bitmap bitmap = ShadowViewDecorator.convertToBitmap(mTextViewHorizontal.getBackground(), 
+						mTextViewHorizontal.getWidth(), mTextViewHorizontal.getHeight());
+				Bitmap bitmpaShadow = mDecorator.createShadow(bitmap, 3, 3, Color.BLACK, 30, 10, false, false, false, true);
+				mTextViewHorizontal.setBackgroundDrawable(new BitmapDrawable(mResources, bitmpaShadow));
 			}
 		});
 	}
